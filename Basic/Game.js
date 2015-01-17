340fd1103dab78003501071427ea7821767042ae
Matthew Hynes <matthew.hynes@mun.ca>
2015-01-17 16:39:51 -0330

    Enemy and bullet velocity

--- Basic/Game.js
+++ Basic/Game.js
@@ -1,16 +1,18 @@
-
 BasicGame.Game = function (game) {
 
-    //  When a State is added to Phaser it automatically has the following properties set on it, even if they already exist:
+    //  When a State is added to Phaser it automatically has the following properties set on it,
+    // even if they already exist:
 
     this.game;      //  a reference to the currently running game (Phaser.Game)
     this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
     this.camera;    //  a reference to the game camera (Phaser.Camera)
     this.cache;     //  the game cache (Phaser.Cache)
-    this.input;     //  the global input manager. You can access this.input.keyboard, this.input.mouse, as well from it. (Phaser.Input)
+    this.input;     //  the global input manager. You can access this.input.keyboard,
+                    // this.input.mouse, as well from it. (Phaser.Input)
     this.load;      //  for preloading assets (Phaser.Loader)
     this.math;      //  lots of useful common math operations (Phaser.Math)
-    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc (Phaser.SoundManager)
+    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc
+                    // (Phaser.SoundManager)
     this.stage;     //  the game stage (Phaser.Stage)
     this.time;      //  the clock (Phaser.Time)
     this.tweens;    //  the tween manager (Phaser.TweenManager)
@@ -21,21 +23,38 @@ BasicGame.Game = function (game) {
     this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
 
     //  You can use any of these from any function within this State.
-    //  But do consider them as being 'reserved words', i.e. don't create a property for your own game called "world" or you'll over-write the world reference.
+    //  But do consider them as being 'reserved words', i.e. don't create a property for your own
+    // game called "world" or you'll over-write the world reference.
 
 };
 
+
 BasicGame.Game.prototype = {
+
 
     create: function () {
 
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        this.gameWidth = this.game.width;
+        this.gameHeight = this.game.height;
+
+        this.sea = this.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, 'sea');
+
+        this.enemy = this.add.sprite(400, 200, 'greenEnemy');
+        this.enemy.animations.add('fly', [0, 1, 2], 20, true);
+        this.enemy.play('fly');
+        this.enemy.anchor.setTo(0.5, 0.5);
+        this.physics.enable(this.enemy, Phaser.Physics.ARCADE);
+
+        this.bullet = this.add.sprite(400, 300, 'bullet');
+        this.bullet.anchor.setTo(0.5, 0.5);
+        this.physics.enable(this.bullet, Phaser.Physics.ARCADE);
+        this.bullet.body.velocity.y = -500
 
     },
 
     update: function () {
 
-        //  Honestly, just about anything could go here. It's YOUR game after all. Eat your heart out!
+        this.sea.tilePosition.y += 0.2;
 
     },
 
