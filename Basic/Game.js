c75a0c2289a807eb95e091890cc3d7511ccda056
Matthew Hynes <matthew.hynes@mun.ca>
2015-01-18 15:20:26 -0330

    Added player-enemy collision detection. Explosions are now a group.

--- Basic/Game.js
+++ Basic/Game.js
@@ -1,173 +1,202 @@
 BasicGame.Game = function (game) {
 
-    //  When a State is added to Phaser it automatically has the following properties set on it,
-    // even if they already exist:
+	//  When a State is added to Phaser it automatically has the following properties set on it,
+	// even if they already exist:
 
-    this.game;      //  a reference to the currently running game (Phaser.Game)
-    this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
-    this.camera;    //  a reference to the game camera (Phaser.Camera)
-    this.cache;     //  the game cache (Phaser.Cache)
-    this.input;     //  the global input manager. You can access this.input.keyboard,
-                    // this.input.mouse, as well from it. (Phaser.Input)
-    this.load;      //  for preloading assets (Phaser.Loader)
-    this.math;      //  lots of useful common math operations (Phaser.Math)
-    this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc
-                    // (Phaser.SoundManager)
-    this.stage;     //  the game stage (Phaser.Stage)
-    this.time;      //  the clock (Phaser.Time)
-    this.tweens;    //  the tween manager (Phaser.TweenManager)
-    this.state;     //  the state manager (Phaser.StateManager)
-    this.world;     //  the game world (Phaser.World)
-    this.particles; //  the particle manager (Phaser.Particles)
-    this.physics;   //  the physics manager (Phaser.Physics)
-    this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
+	this.game;      //  a reference to the currently running game (Phaser.Game)
+	this.add;       //  used to add sprites, text, groups, etc (Phaser.GameObjectFactory)
+	this.camera;    //  a reference to the game camera (Phaser.Camera)
+	this.cache;     //  the game cache (Phaser.Cache)
+	this.input;     //  the global input manager. You can access this.input.keyboard,
+					// this.input.mouse, as well from it. (Phaser.Input)
+	this.load;      //  for preloading assets (Phaser.Loader)
+	this.math;      //  lots of useful common math operations (Phaser.Math)
+	this.sound;     //  the sound manager - add a sound, play one, set-up markers, etc
+					// (Phaser.SoundManager)
+	this.stage;     //  the game stage (Phaser.Stage)
+	this.time;      //  the clock (Phaser.Time)
+	this.tweens;    //  the tween manager (Phaser.TweenManager)
+	this.state;     //  the state manager (Phaser.StateManager)
+	this.world;     //  the game world (Phaser.World)
+	this.particles; //  the particle manager (Phaser.Particles)
+	this.physics;   //  the physics manager (Phaser.Physics)
+	this.rnd;       //  the repeatable random number generator (Phaser.RandomDataGenerator)
 
-    //  You can use any of these from any function within this State.
-    //  But do consider them as being 'reserved words', i.e. don't create a property for your own
-    // game called "world" or you'll over-write the world reference.
+	//  You can use any of these from any function within this State.
+	//  But do consider them as being 'reserved words', i.e. don't create a property for your own
+	// game called "world" or you'll over-write the world reference.
 
 };
 
-
 BasicGame.Game.prototype = {
 
+	create: function () {
 
-    create: function () {
+		this.gameWidth = this.game.width;
+		this.gameHeight = this.game.height;
 
-        this.gameWidth = this.game.width;
-        this.gameHeight = this.game.height;
+		this.sea = this.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, 'sea');
 
-        this.sea = this.add.tileSprite(0, 0, this.gameWidth, this.gameHeight, 'sea');
+		this.player = this.add.sprite(400, 550, 'player');
+		this.player.anchor.setTo(0.5, 0.5);
+		this.player.animations.add('fly', [0, 1, 2], 20, true);
+		this.player.play('fly');
+		this.physics.enable(this.player, Phaser.Physics.ARCADE);
+		this.player.speed = 300;
+		this.player.body.collideWorldBounds = true;
+		this.player.body.setSize(20, 20, 0, -5);
 
-        this.player = this.add.sprite(400, 550, 'player');
-        this.player.anchor.setTo(0.5, 0.5);
-        this.player.animations.add('fly', [0, 1, 2], 20, true);
-        this.player.play('fly');
-        this.physics.enable(this.player, Phaser.Physics.ARCADE);
-        this.player.speed = 300;
-        this.player.body.collideWorldBounds = true;
+		this.enemyPool = this.add.group();
+		this.enemyPool.enableBody = true;
+		this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
+		this.enemyPool.createMultiple(50, 'greenEnemy');
+		this.enemyPool.setAll('anchor.x', 0.5);
+		this.enemyPool.setAll('anchor.y', 0.5);
+		this.enemyPool.setAll('outOfBoundsKill', true);
+		this.enemyPool.setAll('checkWorldBounds', true);
+		this.enemyPool.forEach(function (enemy) {
+			enemy.animations.add('fly', [0, 1, 2], 20, true);
+		});
 
-        this.enemyPool = this.add.group();
-        this.enemyPool.enableBody = true;
-        this.enemyPool.physicsBodyType = Phaser.Physics.ARCADE;
-        this.enemyPool.createMultiple(50, 'greenEnemy');
-        this.enemyPool.setAll('anchor.x', 0.5);
-        this.enemyPool.setAll('anchor.y', 0.5);
-        this.enemyPool.setAll('outOfBoundsKill', true);
-        this.enemyPool.setAll('checkWorldBounds', true);
-        this.enemyPool.forEach(function (enemy) {
-            enemy.animations.add('fly', [0, 1, 2], 20, true);
-        });
+		this.nextEnemyAt = 0;
+		this.enemyDelay = 1000;
 
-        this.nextEnemyAt = 0;
-        this.enemyDelay = 1000;
+		this.bulletPool = this.add.group();
+		this.bulletPool.enableBody = true;
+		this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+		this.bulletPool.createMultiple(100, 'bullet');
+		this.bulletPool.setAll('anchor.x', 0.5);
+		this.bulletPool.setAll('anchor.y', 0.5);
+		this.bulletPool.setAll('outOfBoundsKill', true);
+		this.bulletPool.setAll('checkWorldBounds', true);
 
-        this.bulletPool = this.add.group();
-        this.bulletPool.enableBody = true;
-        this.bulletPool.physicsBodyType = Phaser.Physics.ARCADE;
-        this.bulletPool.createMultiple(100, 'bullet');
-        this.bulletPool.setAll('anchor.x', 0.5);
-        this.bulletPool.setAll('anchor.y', 0.5);
-        this.bulletPool.setAll('outOfBoundsKill', true);
-        this.bulletPool.setAll('checkWorldBounds', true);
+		this.nextShotAt = 0;
+		this.shotDelay = 100;
 
-        this.nextShotAt = 0;
-        this.shotDelay = 100;
+		this.explosionPool = this.add.group();
+		this.explosionPool.enableBody = true;
+		this.explosionPool.physicsBodyType = Phaser.Physics.ARCADE;
+		this.explosionPool.createMultiple(100, 'explosion');
+		this.explosionPool.setAll('anchor.x', 0.5);
+		this.explosionPool.setAll('anchor.y', 0.5);
+		this.explosionPool.forEach(function (explosion) {
+			explosion.animations.add('boom');
+		});
 
-        this.cursors = this.input.keyboard.createCursorKeys();
+		this.cursors = this.input.keyboard.createCursorKeys();
 
-        this.instructions = this.add.text(400, 500, 'Use arrow keys to move\n' + 'Press Z to fire\n' + 'Tapping/clicking does both at once', {
-            font: '20px monspace',
-            fill: '#fff',
-            align: 'center'
-        });
-        this.instructions.anchor.setTo(0.5, 0.5);
-        this.instExpire = this.time.now + 8000;
-    },
+		this.instructions = this.add.text(400, 500, 'Use arrow keys to move\n' + 'Press Z to fire\n' + 'Tapping/clicking does both at once', {
+			font: '20px monspace',
+			fill: '#fff',
+			align: 'center'
+		});
+		this.instructions.anchor.setTo(0.5, 0.5);
+		this.instExpire = this.time.now + 8000;
+	},
 
-    enemyHit: function (bullet, enemy) {
-        bullet.kill();
-        enemy.kill();
+	playerHit: function (player, enemy) {
+		this.explode(enemy);
+		enemy.kill();
 
-        var explosion = this.add.sprite(enemy.x, enemy.y, 'explosion');
-        explosion.anchor.setTo(0.5, 0.5);
-        explosion.animations.add('boom');
-        explosion.play('boom', 15, false, true);
-    },
+		this.explode(player);
+		player.kill();
+	},
 
-    fire: function () {
+	enemyHit: function (bullet, enemy) {
+		bullet.kill();
 
-        if (this.nextShotAt > this.time.now) {
-            return;
-        }
+		this.explode(enemy);
+		enemy.kill();
+	},
 
-        if (this.bulletPool.countDead() === 0) {
-            return;
-        }
+	fire: function () {
 
-        this.nextShotAt = this.time.now + this.shotDelay;
+		if (!this.player.alive || this.nextShotAt > this.time.now) {
+			return;
+		}
 
-        var bullet = this.bulletPool.getFirstExists(false);
-        bullet.reset(this.player.x, this.player.y - 20);
-        bullet.body.velocity.y = -500;
-    },
-    update: function () {
+		if (this.bulletPool.countDead() === 0) {
+			return;
+		}
 
-        this.sea.tilePosition.y += 0.2;
+		this.nextShotAt = this.time.now + this.shotDelay;
 
-        this.physics.arcade.overlap(this.bulletPool, this.enemyPool, this.enemyHit, null, this);
+		var bullet = this.bulletPool.getFirstExists(false);
+		bullet.reset(this.player.x, this.player.y - 20);
+		bullet.body.velocity.y = -500;
+	},
 
-        if (this.nextEnemyAt < this.time.now && this.enemyPool.countDead() > 0) {
-            this.nextEnemyAt = this.time.now + this.enemyDelay;
-            var enemy = this.enemyPool.getFirstExists(false);
-            enemy.reset(this.rnd.integerInRange(20, 1004), 0);
-            enemy.body.velocity.y = this.rnd.integerInRange(30, 60);
-            enemy.play('fly');
-        }
+	explode: function (sprite) {
+		if (this.explosionPool.countDead() === 0) {
+			return;
+		}
 
-        this.player.body.velocity.x = 0;
-        this.player.body.velocity.y = 0;
+		var explosion = this.explosionPool.getFirstExists(false);
+		explosion.reset(sprite.x, sprite.y);
+		explosion.play('boom', 15, false, true);
+		explosion.body.velocity.x = sprite.body.velocity.x;
+		explosion.body.velocity.y = sprite.body.velocity.y;
+	},
 
-        if (this.cursors.left.isDown) {
-            this.player.body.velocity.x = -this.player.speed;
-        }
-        else if (this.cursors.right.isDown) {
-            this.player.body.velocity.x = this.player.speed;
-        }
+	update: function () {
 
-        if (this.cursors.up.isDown) {
-            this.player.body.velocity.y = -this.player.speed;
-        }
-        else if (this.cursors.down.isDown) {
-            this.player.body.velocity.y = this.player.speed;
-        }
+		this.sea.tilePosition.y += 0.2;
 
-        if (this.input.activePointer.isDown && this.physics.arcade.distanceToPointer(this.player) > 5) {
-            this.physics.arcade.moveToPointer(this.player, this.player.speed);
-        }
+		this.physics.arcade.overlap(this.bulletPool, this.enemyPool, this.enemyHit, null, this);
 
-        if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
-            this.fire();
-        }
+		this.physics.arcade.overlap(this.player, this.enemyPool, this.playerHit, null, this);
 
-        if (this.instructions.exists && this.time.now > this.instExpire) {
-            this.instructions.destroy();
-        }
+		if (this.nextEnemyAt < this.time.now && this.enemyPool.countDead() > 0) {
+			this.nextEnemyAt = this.time.now + this.enemyDelay;
+			var enemy = this.enemyPool.getFirstExists(false);
+			enemy.reset(this.rnd.integerInRange(20, 1004), 0);
+			enemy.body.velocity.y = this.rnd.integerInRange(30, 60);
+			enemy.play('fly');
+		}
 
-    },
+		this.player.body.velocity.x = 0;
+		this.player.body.velocity.y = 0;
 
-    render: function () {
+		if (this.cursors.left.isDown) {
+			this.player.body.velocity.x = -this.player.speed;
+		}
+		else if (this.cursors.right.isDown) {
+			this.player.body.velocity.x = this.player.speed;
+		}
 
-    },
+		if (this.cursors.up.isDown) {
+			this.player.body.velocity.y = -this.player.speed;
+		}
+		else if (this.cursors.down.isDown) {
+			this.player.body.velocity.y = this.player.speed;
+		}
 
-    quitGame: function (pointer) {
+		if (this.input.activePointer.isDown && this.physics.arcade.distanceToPointer(this.player) > 5) {
+			this.physics.arcade.moveToPointer(this.player, this.player.speed);
+		}
 
-        //  Here you should destroy anything you no longer need.
-        //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+		if (this.input.keyboard.isDown(Phaser.Keyboard.Z) || this.input.activePointer.isDown) {
+			this.fire();
+		}
 
-        //  Then let's go back to the main menu.
-        this.state.start('MainMenu');
+		if (this.instructions.exists && this.time.now > this.instExpire) {
+			this.instructions.destroy();
+		}
 
-    }
+	},
+
+	render: function () {
+
+	},
+
+	quitGame: function (pointer) {
+
+		//  Here you should destroy anything you no longer need.
+		//  Stop music, delete sprites, purge caches, free resources, all that good stuff.
+
+		//  Then let's go back to the main menu.
+		this.state.start('MainMenu');
+
+	}
 
 };
--- Basic/Preloader.js
+++ Basic/Preloader.js
@@ -1,45 +1,45 @@
 BasicGame.Preloader = function (game) {
 
-    this.background = null;
-    this.preloadBar = null;
+	this.background = null;
+	this.preloadBar = null;
 
-    this.ready = false;
+	this.ready = false;
 
 };
 
 BasicGame.Preloader.prototype = {
 
-    preload: function () {
+	preload: function () {
 
-        this.load.image('sea', 'assets/sea.png');
-        this.load.image('bullet', 'assets/bullet.png');
-        this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
-        this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
-        this.load.spritesheet('player', 'assets/player.png', 64, 64);
+		this.load.image('sea', 'assets/sea.png');
+		this.load.image('bullet', 'assets/bullet.png');
+		this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+		this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
+		this.load.spritesheet('player', 'assets/player.png', 64, 64);
 
-    },
+	},
 
-    create: function () {
+	create: function () {
 
-        this.state.start('Game');
-    },
+		this.state.start('Game');
+	},
 
-    update: function () {
+	update: function () {
 
-        //	You don't actually need to do this, but I find it gives a much smoother game
-        // experience. Basically it will wait for our audio file to be decoded before proceeding to
-        // the MainMenu. You can jump right into the menu if you want and still play the music, but
-        // you'll have a few seconds of delay while the mp3 decodes - so if you need your music to
-        // be in-sync with your menu it's best to wait for it to decode here first, then carry on.
+		//	You don't actually need to do this, but I find it gives a much smoother game
+		// experience. Basically it will wait for our audio file to be decoded before proceeding to
+		// the MainMenu. You can jump right into the menu if you want and still play the music, but
+		// you'll have a few seconds of delay while the mp3 decodes - so if you need your music to
+		// be in-sync with your menu it's best to wait for it to decode here first, then carry on.
 
-        //	If you don't have any music in your game then put the game.state.start line into the
-        // create function and delete the update function completely.
+		//	If you don't have any music in your game then put the game.state.start line into the
+		// create function and delete the update function completely.
 
-        // if (this.cache.isSoundDecoded('titleMusic') && this.ready == false) {
-        //  this.ready = true;
+		// if (this.cache.isSoundDecoded('titleMusic') && this.ready == false) {
+		//  this.ready = true;
 
-        //  }
+		//  }
 
-    }
+	}
 
 };
